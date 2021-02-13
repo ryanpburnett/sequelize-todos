eaf6d12e0a5d80c37624815494b9fcc0f0dd8c99
Ryan <ryanpburnett@yahoo.com>
2021-02-13 12:51:14 -0600

    GET and RENDER tools

--- public/js/view.js
+++ public/js/view.js
@@ -2,6 +2,31 @@ document.addEventListener('DOMContentLoaded', (e) => {
   console.log('DOM loaded!');
   const form = document.getElementById('todo-form')
   const newTodoInput = document.querySelector('input.new-item')
+  const todoListSpan = document.querySelector('.todo-container')
+
+  const getTodos = () => {
+    fetch('/api/todos')
+      .then(response => response.json())
+      .then(todos => renderTodoList(todos))
+  }
+
+  const renderTodoList = (todos) => {
+
+    const todosHTML = todos.map(todo => {
+
+      const li = `<li class="list-group-item todo-item">
+        <span>${todo.text}</span>
+        <input type="text" class="edit" style="display: none;">
+        <button data-id="${todo.id}" class="delete btn btn-danger">x</button>
+        <button data-id="${todo.id}" class="complete btn btn-primary">✓</button>
+      </li>`
+
+      todoListSpan.innerHTML = li
+    }).join('')
+
+      todoListSpan.innerHTML = todosHTML
+  }
+
   form.addEventListener('submit', e => {
     e.preventDefault()
     const text = newTodoInput.value
@@ -13,8 +38,8 @@ document.addEventListener('DOMContentLoaded', (e) => {
         'Content-Type': 'application/json'            
       }
     })
-    .then(response => response.json())
-    .then(json => console.log(json))
+    .then(getTodos)
     .catch(err => console.error(err))
   })
+  getTodos()
 });
