152025c08e8300a22a89caa7f479c31ff4755c10
Ryan <ryanpburnett@yahoo.com>
2021-02-13 11:30:31 -0600

    routes

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const db = require('./models')
+const routes = require('./routes/api-routes.js')
 const PORT = 3000
 
 const app = express()
@@ -9,7 +10,7 @@ app.use(express.json())
 
 app.use(express.static('public'))
 
-// 
+app.use(routes)
 
 db.sequelize.sync().then(() => {
     app.listen(PORT, () => console.log(`App listening on http://localhost:${PORT}`))
